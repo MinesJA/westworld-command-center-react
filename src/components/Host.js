62eee6d344eaec30b46a32ff25efb0d41491bcda
Jonathan Mines <minesja@gmail.com>
2018-11-26 11:12:50 -0500

    Added return statement back to host comp

--- src/components/Host.js
+++ src/components/Host.js
@@ -2,14 +2,17 @@ import React from 'react';
 import '../stylesheets/Host.css'
 import { Card } from 'semantic-ui-react'
 
-const Host = () => (
-  <Card
-    className="host selected"
-    {/* NOTE: The className "host selected" renders a different style than simply "host". */}
-    onClick={ /* On Click what? */}
-    image={/* I wonder what goes here...*/}
-    raised
-  />
-)
+const Host = () => {
+
+  return(
+    <Card
+      className="host selected"
+      {/* NOTE: The className "host selected" renders a different style than simply "host". */}
+      onClick={ /* On Click what? */}
+      image={/* I wonder what goes here...*/}
+      raised
+    />
+  )
+}
 
 export default Host
